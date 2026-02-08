44e2fbaf194fc17d1b933fb879adaed5e34239a0
capJavert <dev@kickass.website>
2026-02-08 20:28:44 +0100

    Revert "fix(android): pull to refresh triggering to often"
    This reverts commit ac44c0fb2a9b9d7326436195fad6c414c3a30992.

--- android/app/src/main/java/eu/jelposkupilo/app/MainActivity.kt
+++ android/app/src/main/java/eu/jelposkupilo/app/MainActivity.kt
@@ -322,9 +322,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun configureRefresh() {
-        swipeRefreshLayout.setOnChildScrollUpCallback { _, _ ->
-            webView.scrollY > 0
-        }
         swipeRefreshLayout.setOnRefreshListener {
             webView.reload()
         }
